35e3fc7400b2f0b885889ef5c6363b945c4b8d14
aishuc@21 <46344241+aishwaryac21@users.noreply.github.com>
2019-09-05 16:30:04 +0530

    Create generatingsequenceandfindingthenthelement.java

---
+++ generatingsequenceandfindingthenthelement.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util;
+class usermaincode
+{
+  public int genseq(int input1,int input2,int input3,int input4)
+  {
+    int a=input2-input1;
+    int b=input3-input2;
+    for(int i=4;i<=input4;i++)
+    {
+      if(i%2==0)
+      {
+        input3+=a;
+      }
+      else
+      {
+        input3+=b;
+      }
+    }
+    return input3;
+  }
+}
+//ip:1,3,6,17
+//op:41(1,3,6,8,11,13,16,18,21,23,26,28...)
